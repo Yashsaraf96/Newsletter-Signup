745fb9ce82132719a69f7cb7a05a3a78e34f97db
Yashsaraf96 <sarafyash2015@gmail.com>
2022-07-17 01:21:05 +0530

    Setup Signup Page using bootstrap example signing page.

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  console.log(req.body);
+  const fname = req.body.firstname;
+  const lname = req.body.lastname;
+  const email = req.body.email;
+  res.send("OK signed in!");
+});
+
 app.listen(3000,function(){
   console.log("Server is running on port 3000");
 });
